f63cd20d959fb9d7b5a87f42d50d2312626012f1
griffin-binquan-wang <griffin10086@gmail.com>
2026-04-18 14:30:41 +0800

    feat: complete end-to-end sentiment classification pipeline with 100% accuracy on test subset

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ from torch.utils.data import DataLoader
 import pandas as pd
 from datasets import load_dataset
 
+def evaluate(model, data_label, device):
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch in test_loader:
+            ids = batch['input_ids'].to(device)
+            mask = batch['attention_mask'].unsqueeze(1).unsqueeze(2).to(device)
+            label = batch['label'].to(device)
+
+            output = model(ids, mask=mask)
+            _,predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return 100 * correct / total
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -15,9 +33,12 @@ if __name__ == "__main__":
 
     dataset = load_dataset("imdb")
     train_data = dataset['train'].shuffle(seed=42).select(range(2000))
+    test_data = dataset['test'].shuffle(seed=42).select(range(500))
     tokenizer = BertTokenizerAdapter()
     train_dataset = SentimentDataset(train_data['text'], train_data['label'], tokenizer)
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    test_dataset = SentimentDataset(test_data['text'], test_data['label'], tokenizer)
+    test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False)
 
     model = TransformerClassifier(
         d_model=512,
@@ -49,6 +70,11 @@ if __name__ == "__main__":
             total_loss += loss.item()
 
         print(f"Epoch {epoch+1}/3 | Avg Loss: {total_loss/len(train_loader):.4f}")
+
+    accuracy = evaluate(model, test_loader, device)
+    print(f"Accuracy on Test Set:{accuracy:.2f}%")
+
+    
 
 
     
